38cd8270c39ac4d88db04e6e3a796b7b74e42c20
Victor <ap.vicespino@gmail.com>
2020-07-09 14:59:58 -0500

    Organización del proyecto.

--- app/src/main/java/com/example/reproductor/main/MainActivity.java
+++ app/src/main/java/com/example/reproductor/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.reproductor;
+package com.example.reproductor.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.reproductor.R;
 
 public class MainActivity extends AppCompatActivity {
 
