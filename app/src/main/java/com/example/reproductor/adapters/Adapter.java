9126b35a1e86cba3bf3607d69310454f7dcc0c60
Victor <ap.vicespino@gmail.com>
2020-07-09 18:38:37 -0500

    Se dectecta la positión actual del PageSnapHelper.

---
+++ app/src/main/java/com/example/reproductor/adapters/Adapter.java
@@ -0,0 +1,75 @@
+package com.example.reproductor.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.reproductor.Models.Song;
+import com.example.reproductor.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Adapter for temporary list of the player,
+ * the one list that is playing currently.
+ */
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+    @SuppressWarnings("unused")
+    private static final String TAG = Adapter.class.getSimpleName();
+
+    private static final int ITEM_COUNT = 20;
+    private List<Song> songList;
+
+    public Adapter() {
+        super();
+
+        // Create some items
+        songList = new ArrayList<>();
+        for (int i = 0; i < ITEM_COUNT; ++i) {
+            songList.add(new Song("The Four Horseman" +i,"Metallica","asd"));
+
+        }
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item_, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Song song = songList.get(position);
+
+        holder.songName.setText(song.getSongName());
+        holder.authorName.setText(song.getAuthor());
+        holder.img.setImageResource(R.drawable.kill_em_all);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return songList.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView songName;
+        TextView authorName;
+        ImageView img;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            songName = itemView.findViewById(R.id.txt_songName);
+            authorName =  itemView.findViewById(R.id.txt_authorName);
+            img=  itemView.findViewById(R.id.img_song);
+        }
+    }
+
+}
--- app/src/main/java/com/example/reproductor/fragments/ReproductorScreen.java
+++ app/src/main/java/com/example/reproductor/fragments/ReproductorScreen.java
@@ -3,13 +3,23 @@ package com.example.reproductor.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.LinearSnapHelper;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.reproductor.R;
+import com.example.reproductor.adapters.Adapter;
 
 
 /**
@@ -17,6 +27,21 @@ import com.example.reproductor.R;
  */
 public class ReproductorScreen extends Fragment {
 
+
+    ImageButton btnNext;
+    ImageButton btnPreviouse;
+    ImageView btnPause;
+    RecyclerView recyclerPlayer;
+    Adapter adapterSongsCurrentPlaying;
+
+    LinearLayoutManager layoutManager =
+            new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false) ;
+
+    //cambio por pagina, aun cuando se haga un scroll rapido, cambiara 1 vez (el lineal, la cantidad cambia por velocidad)
+    SnapHelper snapHelper = new PagerSnapHelper();
+
+    //for knowing the  current position that is been playing.
+    int indexSongCurrent = 0;
 
     public ReproductorScreen() {
         // Required empty public constructor
@@ -27,7 +52,100 @@ public class ReproductorScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reproductor_screen, container, false);
+        View playerView = inflater.inflate(R.layout.fragment_reproductor_screen, container, false);
+        recyclerPlayer = playerView.findViewById(R.id.recycler_songsCurrentlyPlaying);
+        adapterSongsCurrentPlaying = new Adapter();
+        recyclerPlayer.setAdapter(adapterSongsCurrentPlaying);
+
+
+
+
+       // snapHelper.findTargetSnapPosition(layoutManager,10,0);
+       // snapHelper.onFling(10,10);
+        snapHelper.attachToRecyclerView(recyclerPlayer);
+
+
+        recyclerPlayer.setItemAnimator(new DefaultItemAnimator());
+        recyclerPlayer.setLayoutManager(layoutManager);
+
+        btnNext = playerView.findViewById(R.id.btn_nextSong);
+        btnPreviouse = playerView.findViewById(R.id.btn_previousSong);
+        btnPause = playerView.findViewById(R.id.btnPause);
+
+        //make validation for index in range of itemCount
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(indexSongCurrent<adapterSongsCurrentPlaying.getItemCount()){
+                    recyclerPlayer.smoothScrollToPosition(++indexSongCurrent);
+                }
+            }
+        });
+
+        btnPreviouse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(indexSongCurrent>0){
+                    recyclerPlayer.smoothScrollToPosition(--indexSongCurrent);
+                }
+            }
+        });
+
+
+
+
+
+        recyclerPlayer.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                //System.out.println("onscrolled");
+                //Here it's got the targetPosition in base of the scroll,
+                // but, this function is called many times,
+                // implementar function sincrona de acceso a la variable para cambiar la cancion
+               /* int targetPos = snapHelper.findTargetSnapPosition(layoutManager,dx,dy);
+                System.out.println("Target position: "+targetPos);
+                indexSongCurrent=targetPos;
+                */
+               //this makes the same than the code above.
+                //but still have the same problem of calling
+                View findSnapView = snapHelper.findSnapView(layoutManager);
+                int position = layoutManager.getPosition(findSnapView);
+                System.out.println("Last position: " +position );
+            }
+
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                //super.onScrollStateChanged(recyclerView, newState);
+                switch (newState) {
+                    case RecyclerView.SCROLL_STATE_IDLE:
+                        System.out.println("The RecyclerView is not scrolling");
+                        break;
+                    case RecyclerView.SCROLL_STATE_DRAGGING:
+                        System.out.println("Scrolling now");
+                        break;
+                    case RecyclerView.SCROLL_STATE_SETTLING:
+                        System.out.println("Scroll Settling");
+                        //it gives the last position
+                       /*
+                        View findSnapView = snapHelper.findSnapView(layoutManager);
+                        int position = layoutManager.getPosition(findSnapView);
+                        System.out.println("Last position: " +position );
+*/
+                        /*int findFirstCompletelyVisibleItemPosition = layoutManager.findFirstCompletelyVisibleItemPosition();
+                       // Adapter.ViewHolder vieiwH = snapHelper.findSnapView(layoutManager);
+                        System.out.println("firstCompletelyVisibleItemPosition: " + findFirstCompletelyVisibleItemPosition);
+*/
+
+                        break;
+                }
+            }
+        });
+
+        return playerView;
     }
 
 }
