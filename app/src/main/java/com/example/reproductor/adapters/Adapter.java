c2f93756e3854f38d5baf418fa47b496dc8b4563
Victor <ap.vicespino@gmail.com>
2020-07-11 22:05:52 -0500

    Layout CurrentPlayList y song item tipo lista Agregados. Comprobación de tipo de layout en el adaptador del recycler, que ya es usado por 2 recycler.

--- app/src/main/java/com/example/reproductor/adapters/Adapter.java
+++ app/src/main/java/com/example/reproductor/adapters/Adapter.java
@@ -24,11 +24,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     private static final String TAG = Adapter.class.getSimpleName();
 
     private static final int ITEM_COUNT = 20;
+    private String kindView;
     private List<Song> songList;
 
-    public Adapter() {
+    public Adapter(String kindView) {
         super();
 
+        this.kindView = kindView;
         // Create some items
         songList = new ArrayList<>();
         for (int i = 0; i < ITEM_COUNT; ++i) {
@@ -40,7 +42,10 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_item_, parent, false);
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(this.kindView.equals("lista")?R.layout.song_item_lista : R.layout.song_item_,
+                parent, false);
+
         v.findViewById(R.id.img_song).setTransitionName("transition_imgCurrentSong");
         v.findViewById(R.id.txt_songName).setTransitionName("transition_songName");
         v.findViewById(R.id.txt_authorName).setTransitionName("transition_authorName");
